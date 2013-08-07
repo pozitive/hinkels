2894e7e7d128e0063ffb04cb473fee428f507bc5
Dmitry Sukhikh <china@pozitive.biz>
2013-08-07 17:32:30 +0700

    Main page is developed

--- app/controllers/bereau_controller.rb
+++ app/controllers/bereau_controller.rb
@@ -1,8 +1,6 @@
 class BereauController < ApplicationController
   def index
     @articles = Article.order(:title)
-    @services = Service.order(:title)    
-
-    
+    @services = Service.order(:title)
   end
 end

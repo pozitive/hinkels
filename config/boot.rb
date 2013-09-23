f80cd3ec8f3bd9adad279222f748e328faa5d016
Dmitry Sukhikh <china@pozitive.biz>
2013-09-24 02:46:50 +0700

    Nothing mutch

--- config/boot.rb
+++ config/boot.rb
@@ -3,6 +3,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
+
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'

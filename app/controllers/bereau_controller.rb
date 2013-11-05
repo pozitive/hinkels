686c3c396ebd9f093dae3b4a230d35b219aeccec
Dmitry Sukhikh <china@pozitive.biz>
2013-11-05 18:35:39 +0700

    Add activities on the main page

--- app/controllers/bereau_controller.rb
+++ app/controllers/bereau_controller.rb
@@ -2,5 +2,6 @@ class BereauController < ApplicationController
   def index
     @articles = Article.order(:title)
     @services = Service.order(:title)
+    @activities = Activity.order(:created_at).page(params[:page]).per(2)
   end
 end
